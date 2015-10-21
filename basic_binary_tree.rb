3f406f953120444c3ec0165074968583b51ae25a
Becky Skillings <beckyskillings@gmail.com>
2015-10-21 12:57:57 -0600

    refining basic binary tree

--- basic_binary_tree.rb
+++ basic_binary_tree.rb
@@ -7,7 +7,7 @@ class Node
 		@children = []
 		@leaf = false
 		@root = false
-
+		@parent = nil
 	end
 
 end
@@ -18,35 +18,38 @@ def build_tree(values, parent = nil)
 	#base case
 	if values.empty?
 		return
+	#no possible children	
 	elsif values.length == 1
 		current_node = Node.new(values[0])
 		current_node.parent = parent
 		current_node.parent.children.push(current_node)
 		current_node.leaf = true
 		child_values = "leaf"
+	#possible children	
 	else
 		current_index = (values.length / 2) 
 		current_node = Node.new(values[current_index])
 		current_node.parent = parent
 		if parent
 			current_node.parent.children.push(current_node)
+		else 
+			current_node.root = true
+			parent_value = "root"
 		end
 		build_tree(values[0..(current_index - 1)], current_node)
 		build_tree(values[(current_index + 1)..-1], current_node)
 		child_values = []
 		current_node.children.each {|c| child_values.push c.value}
 	end
+	#finding value for printed statement. nothing shows up if I put it in the if earlier
 	if current_node.parent 
 		parent_value = current_node.parent.value 
-	else
-		parent_value = "root"
-		current_node.root = true
 	end
 	print "Value #{current_node.value}, parent #{parent_value}, children #{child_values}\r\n"
 
 
-#check on how attribute accessors work, for how to pass in a parent
-
 end
+
+
 
 build_tree([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14])
