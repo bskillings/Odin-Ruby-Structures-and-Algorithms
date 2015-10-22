db3493918e96f4a274e2393e9a660f05175640a2
Becky Skillings <beckyskillings@gmail.com>
2015-10-22 14:22:03 -0600

    stacking disordered nodes, not inserting

--- node.rb
+++ node.rb
@@ -1,11 +1,10 @@
 class Node
 
-	attr_accessor :parent, :children, :value, :root, :leaf, :ultimate_root
+	attr_accessor :parent, :children, :value, :root
 
 	def initialize (value)
 		@value = value
 		@children = []
-		@leaf = false
 		@root = false
 		@parent = nil
 	end
@@ -18,7 +17,7 @@ class Tree
 		@values = values
 		@first = true
 		@ultimate_root = Node.new(values.shift)
-		puts "creating first node, value #{@ultimate_root.value}"
+		puts "creating root, value #{@ultimate_root.value}"
 		build_tree
 
 	end
@@ -34,54 +33,42 @@ class Tree
 			current_node = Node.new(current_value)
 		#check if it's the first
 		#this part might be recursive
-			put_node_here = where_do_i_belong(current_value, @ultimate_root)
+			new_node_parent = where_do_i_belong(current_value, @ultimate_root)
 			#once you find where it belongs 
 			#hook it up to the parent node
-			current_node.parent = put_node_here
+			current_node.parent = new_node_parent
 			current_node.parent.children.push(current_node)
 
 			#creating the output text
 			parent_value = current_node.parent ? current_node.parent.value : "none"
-			root_value = current_node.parent ? @ultimate_root.value : "none"
-			print "value #{current_value}, parent #{parent_value}, root #{root_value}\r\n"
+			print "value #{current_value}, parent #{parent_value}\r\n"
 		end
 
 	end
 
 	def where_do_i_belong(value, parent)
 
-	if parent.children.length < 2
-			return parent
+		#I think this is where the problem is.  sometimes both nodes are bigger than the root
+		if value <= parent.value
+			chosen_child = parent.children.select {|child| child.value <= parent.value}
 		else
-			if value <= parent.value
-				chosen_child = parent.children.select {|child| child.value <= parent.value}
-			else
-				chosen_child = parent.children.select {|child| child.value > parent.value}
-			end
-			if chosen_child[0].children
-				where_do_i_belong(value, chosen_child[0])
-			else
-				return chosen_child[0]
-			end
-			return 
+			chosen_child = parent.children.select {|child| child.value > parent.value}
 		end
+		if chosen_child.empty?
+			return parent
+		elsif chosen_child.length == 1 && ((value <= parent.value && chosen_child[0].value > parent.value) ||
+				(value > parent.value && chosen_child[0].value <= parent.value))
+				return parent
+		else
+			further_child = where_do_i_belong(value, chosen_child[0])
+			return further_child
+		end
+		return 
 	end
 
 end
 
-#error message end of day oct 21
-
-=begin
-	
-	node.rb:41:in 'block in build_tree': undefined method 'children' for nil:NilClass (NoMethodError)
-	from node.rb:31:in 'each'
-	from node.rb:31:in 'build_tree'
-	from node.rb:22:in 'initialize'
-	from node.rb:114:in 'new'
-	from node.rb:114:in '<main>'
-=end
 
 
-
-
-tree = Tree.new([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14])
+tree = Tree.new([8, 1, 5, 4, 6, 12, 7, 9, 2, 10, 14, 11, 3, 13])
+tree2 = Tree.new([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
