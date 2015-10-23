c62c230f9cbf4a14825a840c4c78c4ab38854af6
Becky Skillings <beckyskillings@gmail.com>
2015-10-23 12:44:16 -0600

    breadth-first and depth-first-nonrecursive working

--- node.rb
+++ node.rb
@@ -35,7 +35,11 @@ class Tree
 		#hook it up to its parent
 		if new_node_parent
 			current_node.parent = new_node_parent
-			current_node.parent.children.push(current_node)
+			if current_node.value <= current_node.parent.value
+				current_node.parent.children.unshift(current_node)
+			else
+				current_node.parent.children.push(current_node)
+			end
 		end
 
 		#if there is more in the array, break it in half
@@ -92,11 +96,58 @@ class Tree
 		end
 	end
 
+	def breadth_first_search(target_value)
+		searching_queue = [@ultimate_root]
+		while target_value != searching_queue[0].value
+			going_to_children = searching_queue.shift
+			going_to_children.children.each {|child| searching_queue.push(child)}
+		end
+	
+		#building the path output string (array)
+		print "#{target_value} found! Here is the breadth-first path: \r\n"
+		building_path = []
+		put_this_in_path = searching_queue[0]
+		building_path.unshift(put_this_in_path.value)
+		while put_this_in_path.parent != nil
+			put_this_in_path = put_this_in_path.parent
+			building_path.unshift(put_this_in_path.value)
+		end
+		print "#{building_path}\r\n"
+	end
+
+	def depth_first_search(target_value)
+		searching_stack = [@ultimate_root]
+		while target_value != searching_stack.last.value
+			test_me = searching_stack.pop
+			if test_me.children
+				test_me.children.reverse.each {|child| searching_stack.push(child)}
+			end
+		end
+
+		#building the path output string (array)
+		print "#{target_value} found! Here is the depth-first path: \r\n"
+		building_path = []
+		put_this_in_path = searching_stack.last
+		building_path.unshift(put_this_in_path.value)
+		while put_this_in_path.parent != nil
+			put_this_in_path = put_this_in_path.parent
+			building_path.unshift(put_this_in_path.value)
+		end
+		print "#{building_path}\r\n"
+	end
 end
 
 
 
 tree = Tree.new([8, 1, 5, 4, 6, 12, 7, 9, 2, 10, 14, 11, 3, 13])
+tree.breadth_first_search(5)
+tree.depth_first_search(5)
+
 tree2 = Tree.new([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14])
+tree2.breadth_first_search(5)
+tree2.depth_first_search(5)
+
 tree3 = Tree.new([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
+tree3.breadth_first_search(5)
+tree3.depth_first_search(5)
 
