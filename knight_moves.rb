9ecdd830fd016eb95d3e933ac9e6947bbc4ec7df
Becky Skillings <beckyskillings@gmail.com>
2015-10-30 10:19:44 -0600

    building tree working, search not working

--- knight_moves.rb
+++ knight_moves.rb
@@ -29,23 +29,120 @@ end
 
 class Knight
 
-	def initialize(x, y)
-		@current_x = x
-		@current_y = y
+	def initialize(current_coordinates, goal_coordinates)
+		@current_coordinates = current_coordinates
+		tree = MoveTree.new(current_coordinates[0], current_coordinates[1])
+		tree.search_for_move(goal_coordinates)
 	end
 
 end
 
 class MoveTree
 	def initialize(x, y)
+		@root = MoveNode.new(x, y)
+		create_acceptable_children(@root)
+	end
 
+	def create_acceptable_children(current_node)
+		potential_children = []
+		x = current_node.location[0]
+		y = current_node.location[1]
+		#find all the theoretically possible moves
+		potential_children.push([x+1, y+2])
+		potential_children.push([x+1, y-2])
+		potential_children.push([x-1, y+2])
+		potential_children.push([x-1, y-2])
+		potential_children.push([x+2, y+1])
+		potential_children.push([x+2, y-1])
+		potential_children.push([x-2, y+1])
+		potential_children.push([x-2, y-1])
+
+		#go down the list of potential children
+		potential_children.each do |child|
+
+			child_is_acceptable = true
+			
+			#reject if off the board
+			if child[0] < 1 || child[0] > 8 or child[1] < 1 or child[1] > 8
+				child_is_acceptable = false
+			end
+
+			#reject if identical values in parent chain
+			unless current_node.parent_value_chain.empty?
+				current_node.parent_value_chain.each do |parent_coordinates|
+					if parent_coordinates[0] == child[0] || parent_coordinates[1] == child[1]
+						child_is_acceptable = false
+					end
+				end
+			end
+					
+			#create the new node
+			if child_is_acceptable == true
+				new_child = MoveNode.new(child[0], child[1])
+				new_child.parent = current_node
+				new_child.parent_value_chain = create_value_chain(current_node).reverse
+				puts "creating new child at #{child[0]}, #{child[1]}, parent chain is #{new_child.parent_value_chain}"
+				current_node.children.push(new_child)
+			end
+		end
+		
+		#recursively create grandchildren &c
+		if current_node.children.empty?
+			return
+		else
+			current_node.children.each do |child_node|
+			create_acceptable_children(child_node)
+			end
+		end
+
+	end
+
+	def create_value_chain(current_parent)
+		value_chain = []
+		value_chain.push(current_parent.location)
+		while current_parent.parent
+			current_parent = current_parent.parent
+			value_chain.push(current_parent.location)
+		end
+		return value_chain
+	end
+
+
+
+	#do a breadth-first search for the goal square
+	#this also isn't working oct 29, 1:00
+	def search_for_move(goal_coordinates)
+		searching_queue = [@root]
+		while goal_coordinates[0] != searching_queue[0].location[0] || goal_coordinates[1] != searching_queue[0].location[0]
+			puts "comparing #{goal_coordinates} to #{searching_queue[0].location}..."
+			going_to_children = searching_queue.shift
+			going_to_children.children.each do |child| 
+				if child != nil
+					searching_queue.push(child)
+				end
+			end
+		end
+		puts "#{goal_coordinates} found! Here is the path:"
+		print searching_queue[0].parent_chain
 	end
 end
 
 class MoveNode
+
+	attr_accessor :location, :children, :parent, :parent_value_chain
 	def initialize(x, y)
-		x_value = x
-		y_value = y
-		children = []
+		@location = [x, y]
+		@children = []
+		@parent = nil
+		@parent_value_chain = []
 	end
 end
+
+knight = Knight.new([4, 4], [5, 6])
+
+#two issues right now (oct 30 10:00):
+
+#if I set it to find something in the first row, it doesn't
+
+#also, I'm getting a nil class error if there are no children, rather than just returning.
+#I think this is when the queue is empty or when something nil gets to the front, not sure which
